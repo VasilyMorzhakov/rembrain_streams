35458b6ef0069bd20d5709075c91cd821c720aec
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-06-10 18:30:01 +0600

    thing_removed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-import 'semantic-ui-css/semantic.min.css'
-
 export { ReactRgbStream } from './components/ReactRgbStream'
 export { ReactResponsiveRgbStream } from './components/ReactResponsiveRgbStream'
