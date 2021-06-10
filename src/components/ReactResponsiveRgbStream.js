82cd41680d850edd7072740235e49799941906e8
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-06-10 18:02:28 +0600

    exprot

--- src/components/ReactResponsiveRgbStream.js
+++ src/components/ReactResponsiveRgbStream.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import { Segment } from 'semantic-ui-react'
 import fit from 'canvas-fit'
 
-const ReactResponsiveRgbStream = ({
+export const ReactResponsiveRgbStream = ({
   token,
   websocketURL,
   robotName,
