c668be168e950671e30f490003ba64874f690230
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-06-22 15:17:56 +0600

    image

---
+++ src/components/RembrainImage.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react/cjs/react.development'
+
+export const RembrainImage = (token, url) => {
+  useEffect(() => {
+    fetch(url, { headers: { Authorization: token } }).then((resp) => {
+      resp.blob().then((blobResp) => {
+        const data = blobResp
+        const urlCreator = window.URL || window.webkitURL
+        document.getElementById('rembrainImage').src =
+          urlCreator.createObjectURL(data)
+      })
+    })
+  }, [token, url])
+  return <img id='rembrainImage' />
+}
