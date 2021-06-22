065fe0efd8588d69542f16c30ecb3378c27d75d5
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-06-22 15:29:42 +0600

    export

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { ReactRgbStream } from './components/ReactRgbStream'
 export { ReactResponsiveRgbStream } from './components/ReactResponsiveRgbStream'
+export { RembrainImage } from './components/RembrainImage'
