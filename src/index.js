2b63e1c907d8647c7d9e0aed015a48e78ae49868
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-07-07 15:55:32 +0600

    path_fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 export { ReactRgbStream } from './components/ReactRgbStream'
 export { ReactResponsiveRgbStream } from './components/ReactResponsiveRgbStream'
 export { RembrainImage } from './components/RembrainImage'
-export {
-  OperatorCanvas,
-  OperatorDebug
-} from './web-operators/src/web-operators'
+export { OperatorCanvas, OperatorDebug } from './web-operators/web-operators'
