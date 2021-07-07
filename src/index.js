8e34a1575e66f72ec2f6eaedc587c478443acf08
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-07-07 16:54:29 +0600

    ts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { ReactRgbStream } from './components/ReactRgbStream'
-export { ReactResponsiveRgbStream } from './components/ReactResponsiveRgbStream'
-export { RembrainImage } from './components/RembrainImage'
+export { RembrainImage } from './image/RembrainImage'
+export { ReactRgbStream } from './streams/ReactRgbStream'
+export { ReactResponsiveRgbStream } from './streams/ReactResponsiveRgbStream'
 export { OperatorCanvas, OperatorDebug } from './web-operators/web-operators'
