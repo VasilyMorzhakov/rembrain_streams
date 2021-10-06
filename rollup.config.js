74bcec197a79305c374937e09e121282f7bfc4ea
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-10-06 15:59:40 +0600

    rollup2

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,27 @@
-import sass from 'rollup-plugin-sass';
-import typescript from 'rollup-plugin-typescript2';
-import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-
+import sass from 'rollup-plugin-sass'
+import typescript from 'rollup-plugin-typescript2'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default [
-    {
-        input: 'src/web-operators.tsx',
-        output: [
-        {
-            file: "dist/web-operators.js",
-            format: 'cjs',
-            exports: 'named',
-            sourcemap: true,
-            strict: false
-        },
-        ],
-        plugins: [
-            sass({ insert: true }),
-            typescript({
-                // include: ["src"],
-                // exclude: ["test/index.tsx"],
-            }),
-        ],
-        external: ['react', 'react-dom', "rxjs", "image-js"]
-    },
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+        strict: false
+      }
+    ],
+    plugins: [
+      sass({ insert: true }),
+      typescript({
+        // include: ["src"],
+        // exclude: ["test/index.tsx"],
+      })
+    ],
+    external: ['react', 'react-dom', 'rxjs', 'image-js']
+  }
 ]
