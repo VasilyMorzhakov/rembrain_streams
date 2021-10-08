85c6c70d2530a2b287fcb8521cf77d30644fbe1d
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-10-08 16:00:09 +0600

    icons_on_buttons

--- src/web-operators/operator-debug.tsx
+++ src/web-operators/operator-debug.tsx
@@ -304,13 +304,17 @@ export class OperatorDebug extends React.Component<IOperatorProps, IOperatorStat
                              <div className="command-item-container">
                                 {this.state.commandList.map(command => 
                                         <div 
-                                            onMouseUp={(e) => {
-                                                if (e.button==2) {
-                                                    this.removeCommand(command.name)
-                                                }
-                                            }}
-                                            className="command-item" onClick={() => this.sendOpClosure(command.op)}>
-                                            {command.name}
+                                            
+                                            className="command-item" onClick={() => 
+                                                this.sendOpClosure(command.op)
+                                            }>
+                                            <span>{command.name}</span>
+                                            <div className="command-item-icon" onClick={(ev) => {
+                                                ev.stopPropagation()
+                                                this.removeCommand(command.name)
+                                            }}>
+                                            <svg width={10} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"></path></svg>
+                                            </div>
                                         </div>
                                         )
                                 }
