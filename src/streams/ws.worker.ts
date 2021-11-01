efbded5a8fc3ed5ec46e9950f8ea355dbdbb59ee
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2021-11-01 14:23:07 +0600

    rebuild

--- src/streams/ws.worker.ts
+++ src/streams/ws.worker.ts
@@ -6,55 +6,57 @@ const wsWorkerCode = () => {
       ws.onopen = () => {
         ws.send(packet)
       }
-
       ws.onmessage = async (ev) => {
-        try {
-          const { data } = ev
-          if (typeof data === 'object') {
-            const dataType = new Uint8Array(
-              await data.slice(0, 1).arrayBuffer()
-            )[0]
-            let imageBlob = null
-            if (dataType === 2) {
-              const lengths = new Uint32Array(
-                await data.slice(1, 13).arrayBuffer()
-              )
-              imageBlob = data.slice(9, 9 + lengths[0])
-            } else if (dataType === 1) {
-              const HEADER_END = 13
-              const lengths = new Uint32Array(
-                await data.slice(1, HEADER_END).arrayBuffer()
-              )
-              imageBlob = data.slice(HEADER_END, HEADER_END + lengths[0])
-            }
-            if (imageBlob) {
-              imageBlob.arrayBuffer().then((val) => {
-                const type = 'image/jpg'
-                const uint8 = new Uint8Array(val)
-                // Chrome thinks that uint8Array is too long to "String.fromCharCode" it,
-                // so it's sliced, stringified and than concated back
-                // It's kinda disgusting but it works
-                let prebtoabuf = ''
-                for (let i = 0; i < 5; i++) {
-                  let n = uint8.length / 5
-                  prebtoabuf += String.fromCharCode.apply(
-                    null,
-                    uint8.slice(n * i, n * (i + 1))
-                  )
-                }
-                const buf = btoa(prebtoabuf)
+        const { data } = ev
+        const dataType = new Uint8Array(await data.slice(0, 1).arrayBuffer())[0]
+        postMessage(`Data type is ${dataType}`)
+        /*
+        if (typeof data === 'object') {
+          const dataType = new Uint8Array(
+            await data.slice(0, 1).arrayBuffer()
+          )[0]
+          let imageBlob = null
 
-                const src = `data:${type};base64,` + buf
-                postMessage({ type: 'image', payload: src })
-              })
-            } else {
-            }
-          } else {
-            postMessage(`Websocket received message: ${data}`)
+          if (dataType === 2) {
+            const lengths = new Uint32Array(
+              await data.slice(1, 13).arrayBuffer()
+            )
+            imageBlob = data.slice(9, 9 + lengths[0])
+          } else if (dataType === 1) {
+            const HEADER_END = 13
+            const lengths = new Uint32Array(
+              await data.slice(1, HEADER_END).arrayBuffer()
+            )
+            imageBlob = data.slice(HEADER_END, HEADER_END + lengths[0])
           }
-        } catch (err) {
-          postMessage({ type: 'error', payload: err })
-        }
+          if (imageBlob) {
+            imageBlob.arrayBuffer().then((val) => {
+              const type = 'image/jpg'
+              const uint8 = new Uint8Array(val)
+              // Chrome thinks that uint8Array is too long to "String.fromCharCode" it,
+              // so it's sliced, stringified and than concated back
+              // It's kinda disgusting but it works
+              let prebtoabuf = ''
+              for (let i = 0; i < 5; i++) {
+                let n = uint8.length / 5
+                prebtoabuf += String.fromCharCode.apply(
+                  null,
+                  uint8.slice(n * i, n * (i + 1))
+                )
+              }
+              const buf = btoa(prebtoabuf)
+
+              const src = `data:${type};base64,` + buf
+              postMessage({ type: 'image', payload: src })
+            })
+          } else {
+            postMessage(
+              `Websocket received object, but its datatype is not 1 or 2: ${data}`
+            )
+          }
+        } else {
+          postMessage(`Websocket received message: ${data}`)
+        }*/
       }
       ws.onclose = (ev) => {
         if (ev.reason !== 'stay down') {
