a32b7b7b7945b4d283f5072ef4bd8798e72b666e
Danil-Dorokhin <dorokhin.d.danil@gmail.com>
2022-02-23 21:19:49 +0600

    stream_resize_thing

--- src/streams/ReactResponsiveRgbStream.tsx
+++ src/streams/ReactResponsiveRgbStream.tsx
@@ -9,7 +9,7 @@ const ReactResponsiveRgbStream = ({
   aspectRatio,
   placeholderText = 'No Image',
   image
-} : {
+}: {
   maxWidth: number,
   minWidth: number,
   aspectRatio: number,
@@ -17,17 +17,31 @@ const ReactResponsiveRgbStream = ({
   image: any,
 }) => {
   const [drawing, setDrawing] = useState(false)
-  const newImg = useRef(new Image(100,100))
-  let resizeTimeout:any
+  const newImg = useRef(new Image(100, 100))
+  const [imageExists, setImageExists] = useState(false)
+  const [resizeImage, setResizeImage] = useState('')
+  const [placeholderImage, setPlaceholderImage] = useState('')
+  let resizeTimeout: any
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
-  
+
   const draw = () => {
     const canvas = canvasRef.current
     if (canvas) {
       const ctx = canvas.getContext('2d')
-      ctx && ctx.drawImage(newImg.current, 1, 1, canvas.width-2, canvas.height-2)
-    }  
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        if (imageExists) {
+          ctx.drawImage(
+            newImg.current,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+          )
+        }
+      }
+    }
   }
 
   const handleResize = () => {
@@ -38,13 +52,94 @@ const ReactResponsiveRgbStream = ({
       if (canvas) {
         fit(canvas)
         setDrawing(true)
-        if (!image) {
-          drawPlaceholder()
-        }
+        draw()
       }
-      
     }, 500)
   }
+
+  useEffect(() => {
+    if (imageExists) {
+      const canvas = canvasRef.current
+      if (canvas) {
+        fit(canvas)
+        setDrawing(true)
+        window.addEventListener('resize', handleResize, false)
+      }
+      return () => {
+        window.removeEventListener('resize', handleResize, false)
+      }
+    }
+  }, [imageExists])
+
+  useEffect(() => {
+    if (image) {
+      newImg.current.src = image
+    } else {
+      setImageExists(false)
+    }
+  }, [image])
+
+  useEffect(() => {
+    newImg.current.onload = () => {
+      setImageExists(true)
+      draw()
+    }
+  }, [imageExists])
+
+  useEffect(() => {
+    if (imageExists) {
+      if (drawing) {
+        setTimeout(() => {
+          setResizeImage('')
+        }, 200)
+      } else {
+        setResizeImage(canvasRef.current.toDataURL())
+      }
+    }
+  }, [drawing, imageExists])
+
+  useEffect(() => {
+    const holderCanvas = document.createElement('canvas')
+    const ctx = holderCanvas.getContext('2d')
+    ctx.fillStyle = '#d3d3d3'
+    ctx.textAlign = 'center'
+    ctx.font = '3em Arial'
+    ctx.textBaseline = 'middle'
+    ctx.fillText(
+      placeholderText,
+      holderCanvas.width / 2,
+      holderCanvas.height / 2
+    )
+    setPlaceholderImage(holderCanvas.toDataURL())
+  }, [placeholderText])
+
+  return (
+    <div
+      style={{
+        aspectRatio: aspectRatio.toString(),
+        maxWidth,
+        minWidth,
+        padding: 0,
+        margin: 0,
+        position: 'relative'
+      }}
+    >
+      {imageExists ?
+        <>
+          <canvas style={drawing ? {} : { display: 'none' }} ref={canvasRef} />
+          <img
+            style={resizeImage ? {} : { display: 'none' }}
+            width="100%"
+            height="100%"
+            src={resizeImage}
+          />
+        </> : <img width="99%" height="99%" src={placeholderImage} />}
+    </div>
+  )
+}
+
+export default WsHOC(ReactResponsiveRgbStream)
+/*
 
   const drawPlaceholder = () => {
     const canvas = canvasRef.current
@@ -64,49 +159,4 @@ const ReactResponsiveRgbStream = ({
       }
     }
   }
-
-  useEffect(() => {
-    const canvas = canvasRef.current
-    if (canvas) {
-      fit(canvas)
-      draw()
-      setDrawing(true)
-      window.addEventListener('resize', handleResize, false)
-    }
-    handleResize()
-    return () => {
-      window.removeEventListener('resize', handleResize, false)
-    }
-  }, [])
-
-  useEffect(() => {
-    if(image) {
-      newImg.current.src = image
-    } else {
-      drawPlaceholder()
-    }
-  }, [image])
-
-  useEffect(() => {
-    newImg.current.onload = () => {
-      draw()
-    }
-  },[])
-
-  return (
-    <div
-      style={{
-        aspectRatio: aspectRatio.toString(),
-        maxWidth,
-        minWidth,
-        padding: 0,
-        margin: 0,
-        position: 'relative'
-      }}
-    >
-      <canvas style={drawing ? {} : { display: 'none' }} ref={canvasRef} />
-    </div>
-  )
-}
-
-export default WsHOC(ReactResponsiveRgbStream)
+*/
